412ea601277cf310b6e33c918eb70844fd68c421
David Dobervich <david.dobervich@gmail.com>
2024-05-26 15:02:04 -0700

    add iris training solution

--- src/main/java/basicneuralnetwork/Main.java
+++ src/main/java/basicneuralnetwork/Main.java
@@ -1,18 +1,15 @@
 package basicneuralnetwork;
 
 import basicneuralnetwork.utilities.FileReaderAndWriter;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 
 public class Main {
     private static final int MAX_EPOCHS = 400;
-    private static double percentForTesting = 0.3;
+    private static double percentForTesting = 0.4;
 
     public static void main(String[] args) {
-
         NeuralNetwork nn = new NeuralNetwork(4, 1,4,3);
 
         ArrayList<double[]> csvData = FileReaderAndWriter.readCSVFile("src/data/iris.data");
@@ -29,28 +26,58 @@ public class Main {
     }
 
     private static void testNN(ArrayList<double[]> test, NeuralNetwork nn) {
-        // TODO: loop over each element of the test data
-        // TODO: extract the feature vector from the row
-        // TODO: run output = nn.guess(...) to get the networks predictions
-        // TODO: find the index of the largest value in output.  That's the guess!
-        // TODO: check if that matches the correct label.  If so add to "correct" counter
-        // TODO: display % correct
+        int correct = 0;
+        int total = 0;
+
+        for (double[] row : test) {
+            double[] input = Arrays.copyOfRange(row, 0, row.length-1);
+            int label = (int)(row[row.length-1]);
+
+            double[] output = nn.guess(input);
+            int guess = getLargestIndex(output);
+            if (guess == label) {
+                correct++;
+            }
+            total++;
+
+            System.out.println("correct: " + correct + "/" + total);
+        }
+    }
+
+    private static int getLargestIndex(double[] output) {
+        int index = 0;
+
+        for (int i = 1; i < output.length; i++) {
+            if (output[i] > output[index]) {
+                index = i;
+            }
+        }
+
+        return index;
     }
 
     private static void batchTrain(ArrayList<double[]> train, NeuralNetwork nn) {
-        // TODO: loop over each row in train
-        // TODO: extract the feature vector you want (for iris, it's the first 4 elements in each row)
-        // TODO: construct the correct output vector (for iris it's a length 3 double array with 1 marked in the index
-        //       for the correct label
-        // TODO: run nn.train(...)
+        for (double[] row : train) {
+            double[] input = Arrays.copyOfRange(row, 0, row.length-1);
+            int label = (int)(row[row.length-1]);
+            double[] correctOutput = new double[] {0,0,0};
+            correctOutput[label]++;
+
+            nn.train(input, correctOutput);
+        }
     }
 
     private static void splitData(ArrayList<double[]> csvData, ArrayList<double[]> train, ArrayList<double[]> test, double percentForTesting){
         Collections.shuffle(csvData);
+        int numTest = (int)(percentForTesting*csvData.size());
 
-        // TODO: calculate # of items from csvData that should get added to test
-        // TODO: add correct # of rows from csvData to test
-        // TODO: add ther est to train
+        for (int i = 0; i < numTest; i++) {
+            test.add(csvData.remove(0));
+        }
+
+        while (csvData.size() > 0) {
+            train.add(csvData.remove(0));
+        }
     }
 
 }
